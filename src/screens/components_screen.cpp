f5527b88a8fb2da4d6d1ea362d91740e9b3a5051
Gerrit 'Geaz' Gazic <geaz@users.noreply.github.com>
2019-10-06 10:43:14 +0200

    Memory Size change

--- src/screens/components_screen.cpp
+++ src/screens/components_screen.cpp
@@ -17,7 +17,7 @@ namespace GGB
     { 
         if(!GGB::ShowComponentsScreen) return;
         ImGui::Begin("Components", &GGB::ShowComponentsScreen, ImGuiWindowFlags_NoResize); 
-        ImGui::SetWindowSize(ImVec2(331, 420), ImGuiCond_Always);
+        ImGui::SetWindowSize(ImVec2(470, 420), ImGuiCond_Always);
         
         ImGui::BeginTabBar("##tabs", ImGuiTabBarFlags_None);
         if(ImGui::BeginTabItem("Memory"))
@@ -62,26 +62,26 @@ namespace GGB
 
         if(ImGui::BeginChild((title + "memory_child").c_str()))
         {
-            int remaining = memorySize % 10;
-            ImGuiListClipper clipper(memorySize / 10 + (remaining > 0 ? 1 : 0));
+            int remaining = memorySize % 16;
+            ImGuiListClipper clipper(memorySize / 16 + (remaining > 0 ? 1 : 0));
             while (clipper.Step())
             {
                 for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
                 {             
-                    ImGui::Text((Helper::IntToHexString((i * 10), 4, false) + " |").c_str()); 
+                    ImGui::Text((Helper::IntToHexString((i * 16), 4, false) + " |").c_str()); 
                     
-                    int bound = remaining > 0 && i == clipper.ItemsCount ? remaining : 10;                
+                    int bound = remaining > 0 && i == clipper.ItemsCount ? remaining : 16;                
                     for(int j = 0; j < bound; j++)
                     {
                         ImGui::SameLine(); 
-                        ImGui::Text(Helper::IntToHexString(memoryReadFunc((i * 10) + j), 2, false).c_str());
+                        ImGui::Text(Helper::IntToHexString(memoryReadFunc((i * 16) + j), 2, false).c_str());
                         if(ImGui::IsItemHovered())
                         {                        
                             ImVec2 pos = ImGui::GetItemRectMin();
                             pos.x -= 4; pos.y -= 4;
                             ImGui::GetWindowDrawList()->AddRectFilled(pos, ImVec2(pos.x + 24, pos.y + 24), IM_COL32(150, 150, 150, 100));
 
-                            std::string tooltip = Helper::IntToHexString((i * 10) + j);
+                            std::string tooltip = Helper::IntToHexString((i * 16) + j);
                             if(toolTipMap.find((i * 10) + j) != toolTipMap.end())
                             {
                                 tooltip += '\n' + toolTipMap.at((i * 10) + j);
@@ -93,7 +93,7 @@ namespace GGB
                 if(scroll_to_value != -1)
                 {
                     // Line Height = 20 px
-                    int scroll_px = 20 * (scroll_to_value / 10 + (scroll_to_value % 10 > 0 ? 1 : 0));
+                    int scroll_px = 20 * (scroll_to_value / 16 + (scroll_to_value % 16 > 0 ? 1 : 0));
                     ImGui::SetScrollFromPosY(ImGui::GetCursorStartPos().y + scroll_px);
                     scroll_to_value = -1;
                 }
